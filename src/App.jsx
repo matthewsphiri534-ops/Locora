727cc1304e65fe7035e1c469d035942153622450
matthewsphiri534-ops <matthewsphiri534@gmail.com>
2026-03-24 02:59:33 +0200

    Create App.jsx file in the src directory

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello, world!</h1>
+      <p>Welcome to Locora.</p>
+    </div>
+  );
+}
+
+export default App;
